73cbec695c2be1fee008863fa3c81177ab77e0b6
Lola Castan <lcastan.pro@gmail.com>
2025-04-14 17:55:27 +0200

    feat: add SonarQube scanner dependency and configuration refactor: clean up auth and users controllers fix: update .gitignore to include /http

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,38 +1,38 @@
 
 import {
-    Body,
-    Controller,
-    Get,
-    HttpCode,
-    HttpStatus,
-    Post,
-    Request,
-    UseGuards
-  } from '@nestjs/common';
-  import { AuthGuard } from './auth.guard';
-  import { AuthService } from './auth.service';
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
 import { Public } from './decorators/public';
-  
-  @Controller('auth')
-  export class AuthController {
-    constructor(private authService: AuthService) {}
-  
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    async signIn(@Body() signInDto: Record<string, any>) {
-      return this.authService.signIn(signInDto.pseudo, signInDto.password);
-    }
-  
-    @UseGuards(AuthGuard)
-    @Get('profile')
-    getProfile(@Request() req: any) { //changer ANY 
-      return req.user; 
-    }
 
-    @Public()
-    @Post('verify-token')
-    verifyToken(@Body() body: { token: string }) {
-      return this.authService.verifyToken(body.token);
-    }
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  @Public()
+  async signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.pseudo, signInDto.password);
   }
-  
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) { //changer ANY 
+    return req.user; 
+  }
+
+  @Public()
+  @Post('verify-token')
+  verifyToken(@Body() body: { token: string }) {
+    return this.authService.verifyToken(body.token);
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,25 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Public } from '../auth/decorators/public';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {
   }
+
   @Get()
   getUsers(): any {
-      return this.usersService.getAllUsers();
+    return this.usersService.getAllUsers();
   }
 
-  
   @Get(':id')
   getUserById(@Param('id') id: number): any {
     return this.usersService.getUserById(id);
   }
 
   @Post()
+  @Public()
   create(@Body() body: CreateUserDto) {
     console.log("create user")
     return this.usersService.create(body);
@@ -29,7 +31,6 @@ export class UsersController {
   //   @Body() UpdateUserDto: UpdateUserDto) {
   //   return this.usersService.updatebank(id, UpdateUserDto.bank);
   // }
-
 }
 
 
