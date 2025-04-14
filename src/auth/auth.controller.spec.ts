379585e845aed6887e0e5f85cdd02737d5d91fe7
chapplallie <156299381+chapplallie@users.noreply.github.com>
2025-04-14 17:24:07 +0200

    fix: make initializeDeck public and reinitialize deck at each round

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -3,7 +3,6 @@ import { AuthController } from './auth.controller';
 
 describe('AuthController', () => {
   let controller: AuthController;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
