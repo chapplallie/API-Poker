bd5722043b1dfd8377f90b0e57eab6cf46cabda4
Lallie.chappon <chapplallie@gmail.fr>
2025-04-29 15:16:10 +0200

    add: minimal front

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggerInterceptorInterceptor } from './logger-interceptor/logger-interceptor.interceptor';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express'; // Import NestExpressApplication
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Create the app as a NestExpressApplication
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors();
+
+  // Serve static files from the "public" folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const config = new DocumentBuilder()
     .setTitle('poker API')
@@ -16,10 +23,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-
-  app.useGlobalInterceptors(new LoggerInterceptorInterceptor())
+  app.useGlobalInterceptors(new LoggerInterceptorInterceptor());
   await app.listen(process.env.PORT ?? 3000);
   console.log('API is running on http://localhost:' + (process.env.PORT ?? 3000));
-  
 }
 bootstrap();
