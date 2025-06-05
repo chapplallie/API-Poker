d3fc784ec624383b43a43e2e9e7ac2862557b6db
Lola Castan <lcastan.pro@gmail.com>
2025-06-05 09:50:15 +0200

    fix: ajouter fichier de config pour Jest et modifier le fichier de test pou rle usersService

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+module.exports = {
+  // ...autres configs Jest
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+};
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { ActionsService } from '../actions/actions.service';
+import { PlayersService } from '../players/players.service';
+import { TablesService } from '../tables/tables.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    // const module: TestingModule = await Test.createTestingModule({
+    //   providers: [UsersService],
+    // }).compile();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    providers: [
+      ActionsService,
+      {
+        provide: PlayersService,
+        useValue: {}// mockPlayersService, // ou simplement un objet vide `{}` si pas encore simulé
+      },
+      {
+        provide: TablesService,
+        useValue: {}, // mock ou service réel si nécessaire
+      },
+    ],
+  }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
